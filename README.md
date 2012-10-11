baf43725d09b638005ec29b7ed2f9f5ddae16045
cjwebb <colinjameswebb@hotmail.com>
2012-10-11 09:33:30 +0100

    upon startup, create a pool of available users.

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 facebook-test-users
 ===================
+
+This is currently in development. Please don't complain when it screws up.
+
+Things to do:
+ * refactoring duplicate code.
+ * retries when Facebook returns error
+ * useful errors upon OAuthExceptions
+ * delete all endpoint
--- server.js
+++ server.js
@@ -7,6 +7,11 @@ redisClient.on("error", function (err) {
 	console.log("Error " + err);
 });
 
+var redisClient2 = redis.createClient();
+redisClient2.on("error", function (err) {
+	console.log("Error " + err);
+});
+
 var app = express();
 app.get('/callfb', requestHandlers.callFacebook);
 app.get('/', requestHandlers.getAUser);
@@ -20,8 +25,28 @@ app.del('/facebook/user/:id', requestHandlers.deleteUser);
 var facebook = require('./facebookFunctions');
 redisClient.on("message", function (channel, message) {
 	console.log(message);
-	facebook.createNewUser();
+	facebook.createNewUser(function (user){
+		redisClient2.hmset("user:"+newUser.id, "access_token", newUser.access_token);
+		redisClient2.lpush("users", "user:"+newUser.id);
+	});
 });
 redisClient.subscribe("user_requested");
 
+// on startup, go and fetch some users
+NUMBER_OF_USERS_TO_HAVE_AVAILABLE = 10; // TODO: config
+redisClient2.llen("users", function(err, numberOfUsers) {
+	console.log("Startup: "+numberOfUsers+" users currently available");
+	var usersNeeded = NUMBER_OF_USERS_TO_HAVE_AVAILABLE - numberOfUsers;
+	if (usersNeeded > 0) {
+		console.log("Startup: Fetching "+usersNeeded+" users");
+		for (i=0; i<usersNeeded; i++) {
+			facebook.createNewUser(function(user){
+				redisClient2.hmset("user:"+newUser.id, "access_token", newUser.access_token);
+				redisClient2.lpush("users", "user:"+newUser.id);
+			});
+		}
+	}
+});
+
+console.log("Startup: Listening on 127.0.01:1337");
 app.listen(1337);
